437c534e6b7b92fb5e447837b230665aa540b5de
zhouzhihui <zhouzhihui@htgames.cn>
2017-07-06 13:37:53 +0800

    英雄飞机动画的动画处理

--- planeWar/src/app.js
+++ planeWar/src/app.js
@@ -25,8 +25,11 @@ var GameLayer = cc.Layer.extend({
         // this.setMouseEnabled(true);
         // this.setKeyboardEnabled(true);//We have delete these three functions from layer in v3.0.
         var size = cc.winSize;
+
+        var deleteColor = new cc.LayerColor(cc.color(255, 255, 0, 100), size.width, size.height);
         this.planeLayer = new PlaneLayer();
-        this.addChild(this.planeLayer);
+        deleteColor.addChild(this.planeLayer);
+        this.addChild(deleteColor);
     }
 });
 
--- planeWar/src/planeLayer.js
+++ planeWar/src/planeLayer.js
@@ -20,6 +20,15 @@ var PlaneLayer = cc.Layer.extend({
         plane.setPosition(size.width / 2, plane.getContentSize().height / 2);
         plane.attr = PlanCfg.newPlane();
         this.addChild(plane, 0, AIRPLANE);
+        //动画
+        var blink = cc.Blink.create(1, 3);
+        var animation =  cc.Animation.create();
+        animation.setDelayPerUnit(0.1);
+        animation.addSpriteFrame(cc.spriteFrameCache.getSpriteFrame("hero1.png"));
+        animation.addSpriteFrame(cc.spriteFrameCache.getSpriteFrame("hero2.png"));
+        var animate = cc.Animate.create(animation);
+        plane.runAction(blink);
+        plane.runAction(cc.RepeatForever.create(animate));
     },
     blowUp: function () {
         this.isAlive = false;
