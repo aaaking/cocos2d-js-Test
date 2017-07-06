a6ec91917c011d7c90e4b763d499fbb9bb0ad631
zhouzhihui <zhouzhihui@htgames.cn>
2017-07-06 14:37:21 +0800

    背景图片的设置

--- planeWar/src/app.js
+++ planeWar/src/app.js
@@ -7,6 +7,8 @@ var HelloWorldScene = cc.Scene.extend({
 });
 
 var GameLayer = cc.Layer.extend({
+    background1:null,
+    background2:null,
     planeLayer: null,
     bulletLayer: null,
     enemyLayer: null,
@@ -25,15 +27,24 @@ var GameLayer = cc.Layer.extend({
         // this.setMouseEnabled(true);
         // this.setKeyboardEnabled(true);//We have delete these three functions from layer in v3.0.
         var size = cc.winSize;
-        var deleteColor = new cc.LayerColor(cc.color(255, 255, 0, 100), size.width, size.height);
+        cc.spriteFrameCache.addSpriteFrames(res.s_ShootBackgroundList, res.s_ShootBackground);
+        this.background1 = cc.Sprite.create("#background.png");
+        this.background1.setAnchorPoint(0, 0);
+        this.background1.setScaleX(size.width / this.background1.getContentSize().width);
+        this.background1.setScaleY(size.height / this.background1.getContentSize().height);
+        this.addChild(this.background1);
+        this.background2 = cc.Sprite.create("#background.png");
+        this.background2.setAnchorPoint(0, 0);
+        this.background2.setScaleX(size.width / this.background2.getContentSize().width);
+        this.background2.setScaleY(size.height / this.background2.getContentSize().height);
+        this.addChild(this.background1);
         //飞机
         this.planeLayer = new PlaneLayer();
-        deleteColor.addChild(this.planeLayer);
-        this.addChild(deleteColor);
+        this.addChild(this.planeLayer);
         //子弹
         this.bulletLayer = new BulletLayer(this.planeLayer.getChildByTag(AIRPLANE));
         this.bulletLayer.startShoot();
-        deleteColor.addChild(this.bulletLayer);
+        this.addChild(this.bulletLayer);
     }
 });
 
