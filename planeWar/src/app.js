43f1cd90be934410952e892dd8c39054e0283b6b
zhouzhihui <zhouzhihui@htgames.cn>
2017-07-06 11:28:11 +0800

    add some code

--- planeWar/src/app.js
+++ planeWar/src/app.js
@@ -1,45 +1,42 @@
 
-var HelloWorldLayer = cc.Layer.extend({
-    sprite:null,
-    ctor:function () {
-        //////////////////////////////
-        // 1. super init first
-        this._super();
-
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
-        var size = cc.winSize;
-
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
-        // position the label on the center of the screen
-        helloLabel.x = size.width / 2;
-        helloLabel.y = size.height / 2 + 200;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
-
-        // add "HelloWorld" splash screen"
-        this.sprite = new cc.Sprite(res.HelloWorld_png);
-        this.sprite.attr({
-            x: size.width / 2,
-            y: size.height / 2
-        });
-        this.addChild(this.sprite, 0);
-
-        return true;
-    }
-});
-
 var HelloWorldScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-        var layer = new HelloWorldLayer();
+        var layer = new GameLayer();
         this.addChild(layer);
+    }
+});
+
+var GameLayer = cc.Layer.extend({
+    background1: null,
+    background2: null,
+    planeLayer: null,
+    bulletLayer: null,
+    enemyLayer: null,
+    controlLayer: null,
+    bgPosY: 0,
+    bgHeight: 0,
+    score: 0,
+    ctor: function () {
+        this._super();
+        this.init();
+        return true;
+    },
+
+    init: function () {
+        // this.setTouchEnabled(true);
+        // this.setMouseEnabled(true);
+        // this.setKeyboardEnabled(true);
+        var size = cc.winSize;
+        var deleteLayer = new cc.LayerColor(cc.color(100, 200, 100, 255), size.width, size.height);
+
+        var deleteLabel = new cc.LabelTTF("Hello", "Arial", 60);
+        deleteLabel.fillStyle = cc.color.GREEN;
+        deleteLabel.setAnchorPoint(0, 0);
+        deleteLabel.setPosition(size.width / 2, size.height / 2);
+        deleteLayer.addChild(deleteLabel);
+
+        this.addChild(deleteLayer, 0);
     }
 });
 
