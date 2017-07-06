d6088767d0b1e1c462d88572162b219017eeb1e4
zhouzhihui <zhouzhihui@htgames.cn>
2017-07-06 20:49:17 +0800

    暂停按钮的点击事件优化

--- planeWar/src/controlLayer.js
+++ planeWar/src/controlLayer.js
@@ -9,14 +9,13 @@ var ControlLayer = cc.Layer.extend({
     ctor: function () {
         this._super();
         this.init();
-        this.loadListener();
     },
 
     init: function () {
         var winSize = cc.director.getWinSize();
         var norPause = cc.Sprite.create("#game_pause_nor.png");
         var pressPause = cc.Sprite.create("#game_pause_pressed.png");
-        this.pauseItem = cc.MenuItemSprite.create(norPause, pressPause, null, null, this);
+        this.pauseItem = cc.MenuItemSprite.create(norPause, pressPause, null, this.menuPauseCallback, this);
         //pauseItem.initWithNormalSprite();
         this.pauseItem.setPosition(cc.p(norPause.getContentSize().width / 2 + 10, winSize.height - norPause.getContentSize().height / 2 - 10));
         var menuPause = cc.Menu.create(this.pauseItem);
@@ -41,40 +40,6 @@ var ControlLayer = cc.Layer.extend({
 //            this.addChild(hiScoreItem);
 //        }
 
-    },
-    loadListener: function () {
-        var listener = cc.EventListener.create({
-            event: cc.EventListener.TOUCH_ONE_BY_ONE,
-            target: this.pauseItem,
-            swallowTouches: true,//不向下触摸，简单点来说，比如有两个sprite ,A 和 B，A在上B在下（位置重叠），触摸A的时候，B不会受到影响
-            onTouchBegan: this.onTouchBegan,
-            onTouchMoved: this.onTouchMoved,
-            onTouchEnded: this.onTouchEnded
-        });
-        cc.eventManager.addListener(listener, this.pauseItem);
-    },
-    onTouchBegan: function (touch , event) {
-        var self = this.target;
-        var touchPoint = touch.getLocation();
-        var locationInNode = self.convertToNodeSpace(touchPoint);
-        var rect = cc.rect(0, 0, self.getContentSize().width, self.getContentSize().height);
-        if (cc.rectContainsPoint(rect, locationInNode)) {
-            return true;
-        }
-        return false;
-    },
-    onTouchMoved: function (touch, event) {
-
-    },
-    onTouchEnded: function (touch, event) {
-        var self = this.target;
-        var touchPoint = touch.getLocation();
-        var locationInNode = self.convertToNodeSpace(touchPoint);
-        var rect = cc.rect(0, 0, self.getContentSize().width, self.getContentSize().height);
-        if (!cc.rectContainsPoint(rect, locationInNode)) {
-            return;
-        }
-        self.getParent().getParent().menuPauseCallback();
     },
     menuPauseCallback: function (obj) {
         var director = cc.director;
